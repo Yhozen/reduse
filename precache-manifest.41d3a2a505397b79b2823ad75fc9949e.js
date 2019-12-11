a8823685c913bad26c39994545d64f15a1b87b19
Gabriel Pérez <gabriel@garox.org>
2019-12-11 10:29:59 -0300

    Updates

---
+++ precache-manifest.41d3a2a505397b79b2823ad75fc9949e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39b5256c38c4419b8d7d18190e83c8b8",
+    "url": "/reduse/index.html"
+  },
+  {
+    "revision": "effe85dd6d661678a55e",
+    "url": "/reduse/static/css/main.cbc15cfc.chunk.css"
+  },
+  {
+    "revision": "88db6ae02f198f46eff7",
+    "url": "/reduse/static/js/2.9335c2a6.chunk.js"
+  },
+  {
+    "revision": "ab811221b84dccf92df78f0dce9a44cf",
+    "url": "/reduse/static/js/2.9335c2a6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "effe85dd6d661678a55e",
+    "url": "/reduse/static/js/main.d07cb73a.chunk.js"
+  },
+  {
+    "revision": "e9c56541cfe48de8fa56",
+    "url": "/reduse/static/js/runtime-main.d8055c4c.js"
+  }
+]);
